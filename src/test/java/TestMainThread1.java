5b2bff16bca8c131823774367f3467713ce3bbd2
ChangcunKang <changcunkang@fico.com>
2018-04-01 12:25:49 +0800

    更新

--- src/test/java/TestMainThread1.java
+++ src/test/java/TestMainThread1.java
@@ -3,6 +3,7 @@ import com.cams.blaze.service.TestDataQueryService;
 import com.fico.testCaseGenerator.XSTream.XSTreamHelper;
 import com.fico.testCaseGenerator.converter.TestDataConvertProcessor;
 import com.fico.testCaseGenerator.repository.ApplicationDao;
+import com.fico.testCaseGenerator.repository.BlazApCuCustrDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,14 +14,19 @@ public class TestMainThread1 {
     public static void main(String[] args) throws Exception{
         TestDataQueryService testDataQueryService = new TestDataQueryService();
 
-        Application application = testDataQueryService.findApplicationByID(4670);
+        Application application = testDataQueryService.findApplicationByID(16);
 
-        String applicationXMLStr = testDataQueryService.findApplicationXMLStringByID(4670);
+        String applicationXMLStr = testDataQueryService.findApplicationXMLStringByID(16);
 
         TestDataConvertProcessor testDataConvertProcessor = new TestDataConvertProcessor();
 
         List obj = testDataConvertProcessor.process( application );
 
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext-batch.xml");
+   //     ApplicationDao appDao = (ApplicationDao) context.getBean(ApplicationDao.class);
+        BlazApCuCustrDao blazDao = (BlazApCuCustrDao)context.getBean(BlazApCuCustrDao.class);
+        blazDao.save(obj);
         System.out.println("");
     }
 }
